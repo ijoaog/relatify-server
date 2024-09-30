8a07e4ba0f9bf051ad7015587888df177ad043b2
João Gabriel Klein <jgkl_25@hotmail.com>
2024-09-30 17:16:22 -0300

    feat: configurar entidades, banco e middleware de autenticalçao com bearer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,32 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config'; // Adiciona o ConfigModule e ConfigService
+import { ConfigModule, ConfigService } from '@nestjs/config'; 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MonitoredIndividualsModule } from './monitored_individuals/monitored_individuals.module';
 import { AnkletMonitoringModule } from './anklet_monitoring/anklet_monitoring.module';
-import { MonitoringReport } from './anklet_monitoring/entities/anklet_monitoring.entity'; // Importe a entidade MonitoringReport
-import { User } from './users/entities/user.entity'; // Importe a entidade User
-import { Detainee } from './monitored_individuals/entities/monitored_individual.entity'; // Importe a entidade Detainee
+import { MonitoringReport } from './anklet_monitoring/entities/anklet_monitoring.entity';
+import { User } from './users/entities/user.entity';
+import { Detainee } from './monitored_individuals/entities/monitored_individual.entity';
+import { AuthMiddleware } from './auth/middleware/auth.middleware';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // Torna o ConfigModule global para toda a aplicação
+    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: configService.get<'mysql' | 'mariadb' | 'postgres' | 'sqlite' | 'mssql' | 'oracle'>('DB_TYPE'), // Defina os tipos suportados
+        type: configService.get<'mysql' | 'mariadb' | 'postgres' | 'sqlite' | 'mssql' | 'oracle'>('DB_TYPE'),
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
-        entities: [MonitoringReport, User, Detainee], // Adicionando as entidades
+        entities: [MonitoringReport, User, Detainee],
         synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
         logging: configService.get<boolean>('DB_LOGGING'),
       }),
@@ -37,4 +38,11 @@ import { Detainee } from './monitored_individuals/entities/monitored_individual.
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes('*');
+  }
+}
---
+++ src/auth/middleware/auth.middleware.ts
@@ -0,0 +1,21 @@
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  private readonly staticToken = process.env.BEARER_TOKEN;
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader || authHeader !== `Bearer ${this.staticToken}`) {
+      throw new UnauthorizedException('Token inválido ou não fornecido');
+    }
+
+    next();
+  }
+}
