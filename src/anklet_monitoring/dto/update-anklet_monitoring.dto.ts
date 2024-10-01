073716e40bb8117dcc3bc84000ad17ff1f2d02db
João Gabriel Klein <jgkl_25@hotmail.com>
2024-09-30 21:42:14 -0300

    feat: remover arquivos nao usados

--- src/anklet_monitoring/dto/update-anklet_monitoring.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAnkletMonitoringDto } from './create-anklet_monitoring.dto';
-
-export class UpdateAnkletMonitoringDto extends PartialType(CreateAnkletMonitoringDto) {}
--- src/monitored_individuals/dto/update-monitored_individual.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMonitoredIndividualDto } from './create-monitored_individual.dto';
-
-export class UpdateMonitoredIndividualDto extends PartialType(CreateMonitoredIndividualDto) {}
